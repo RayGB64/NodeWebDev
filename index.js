39bd39caa47592656341eb5a30e1094ca9093c32
Rayan Abroudjameur <a016132n@student.staffs.ac.uk>
2025-03-24 14:19:15 +0000

    Activity5Client

--- index.js
+++ index.js
@@ -1,35 +1,49 @@
 // Directives
 "use strict";
-// Imports
-import { WebSocket } from "ws";
-// Objects
-const sock = new WebSocket("ws://127.0.0.1:8080");
-// Consts
-const pingMSInterval = 1000;
-
-// Event Listeners
-sock.on("close", (code, buff) =>
+// Namespace
+const chatClientNS = (() =>
 {
-    console.log(`Connection Closed!\nCode: ${code}\nReason: ${buff}`);
-});
-
-sock.on("open", () =>
-{
-    console.log("Connection Opened!");
-});
-
-sock.on("pong" , (data) =>
-{
-    console.log(`Received: ${data}`);
-});
-
-sock.on("error", (err) =>
-{
-    console.error(err);
-});
-
-// Script
-setInterval(() =>
-{
-    sock.ping("ping", true);
-}, pingMSInterval);
+// References
+    const formMsg = document.getElementById("form-message");
+    const msgField = document.getElementById("form-message").elements["message"];
+    const nameField = document.getElementById("form-message").elements["name"];
+    const msgBox = document.getElementById("simple-messagebox");
+// Object
+    const ws = new WebSocket("ws://127.0.0.1:8080");
+// Events
+    ws.addEventListener("close", (clsEvnt) =>
+    {
+        msgBox.innerHTML += `<em>Closed with code: <strong>${clsEvnt.code}</strong> and status:
+        <strong>${clsEvnt.reason}</strong></em><br>`;
+    });
+    ws.addEventListener("error", (event) =>
+    {
+        msgBox.innerHTML += `<em>Error <strong>has occured</strong></em><br>`;
+    });
+    ws.addEventListener("message", (msgEvnt) =>
+    {
+// Local Const
+        const message = JSON.parse(msgEvnt.data);
+        msgBox.innerHTML += `<time>${message.timestamp}</time> - ${message.name}:
+        ${message.message}<br>`;
+    });
+    ws.addEventListener("open", () =>
+    {
+        msgBox.innerHTML += `<em>Connected!</em><br>`;
+    });
+    formMsg.addEventListener("submit", (submitEvnt) =>
+    {
+// Local Const
+        const jsonPayload =
+        {
+            "name" : nameField.value,
+            "message" : msgField.value
+        };
+// Prevent Default Behaviour (submitting a form, see URL with/without)
+        submitEvnt.preventDefault();
+// Make use of the string data param
+        ws.send(JSON.stringify(jsonPayload));
+    });
+// Script Start
+    msgBox.innerHTML += "<em>Loading...</em><br>";
+})()
