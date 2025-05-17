8661bcefa1083e80873e513374297995080555b1
Rayan <rayanabroudj4@gmail.com>
2025-05-17 12:34:27 +0100

    InitialCommit

--- index.js
+++ index.js
@@ -1,50 +1,99 @@
-// Directives
+/* Directive */
 "use strict";
-// Imports
-import { WebSocket, WebSocketServer } from "ws";
-// Consts
-const port = 8080;
-// Object
-const wsServer = new WebSocketServer( {port: port} );
 
-wsServer.on("error", (err) =>
+/* Namespace */
+const ns_client = (() =>
 {
-    console.error(`Server encountered an error: ${err}`);
-});
+    /* References */
+    const statusBox = document.getElementById("status-box");
+    const subForm = document.getElementById("sub-form");
+    const subNaptan = document.getElementById("sub-form").elements["naptan-sub"];
+    const unsubForm = document.getElementById("unsub-form");
+    const unsubNaptan = document.getElementById("unsub-form").elements["naptan-unsub"];
 
-wsServer.on("listening", () =>
-{
-    console.log(`Server started successfully at port: ${port}`);
-});
+    /* Client Init */
+    const sock = new WebSocket("ws://127.0.0.1")
 
-wsServer.on("connection", (sock) =>
-{
-    console.log("New client connected!");
-    // Client Events
-    sock.on("close", (code, reason) =>
+    /* Script Start */
+    statusBox.innerHTML += `Loading...<br>`;
+
+/************************************************************************************** FORM EVENTS */
+    subForm.addEventListener("submit", (submitEvnt) =>
     {
-        console.log(`Client has disconnected with code: ${code} and reason: ${reason}`);
+        // Send sub/unsub to middleware
+        const data =
+        {
+            msgType: "cmd",
+            payload:
+            {
+                msg: "sub",
+                topic: subNaptan.value
+            }
+        }
+
+        // Prevent Default Behaviour (submitting a form, see URL with/without)
+        submitEvnt.preventDefault();
+
+        console.log(data.payload.msg);
+        sock.send(JSON.stringify(data));
     });
     
-    sock.on("message", (msgData) =>
+    unsubForm.addEventListener("submit", (submitEvnt) =>
     {
-        // Local Variables
-        let jsonMsg;
-        let broadcastPayload;
-        // Parsing
-        msgData = msgData.toString();
-        jsonMsg = JSON.parse(msgData);
-        // Add server timestamp
-        jsonMsg.timestamp = new Date().toLocaleString("en-GB");
-        // Prep for broadcast
-        broadcastPayload = JSON.stringify(jsonMsg);
-        // Broadcoast: https://github.com/websockets/ws/tree/master#server-broadcast
-        wsServer.clients.forEach((client) =>
+        // Send sub/unsub to middleware
+        const data =
         {
-            if(client.readyState === WebSocket.OPEN)
+            msgType: "cmd",
+            payload:
             {
-                client.send(broadcastPayload);
+                msg: "unsub",
+                topic: unsubNaptan.value
             }
-        });
+        }
+
+        // Prevent Default Behaviour (submitting a form, see URL with/without)
+        submitEvnt.preventDefault();
+
+        console.log(data.payload.msg);
+        sock.send(JSON.stringify(data));
     });
-});
+
+/********************************************************************************* WEBSOCKET EVENTS */
+    sock.addEventListener("close", (clsEvnt) =>
+    {
+        statusBox.innerHTML += `Closed with code: ${clsEvnt.code}<br>`;
+    });
+
+    sock.addEventListener("message", (msgEvent) =>
+    {
+        // Parse it
+        const message = JSON.parse(msgEvent.data);
+        // handle sub/unsub
+        switch(message.msgType)
+        {
+            case "cmd":
+                switch(message.payload.msg)
+                {
+                    case "sub":
+                        statusBox.innerHTML += `Subscribed to ${message.payload.topic}<br>`;
+                        break;
+                    case "unsub":
+                        statusBox.innerHTML += `Unsubscribed from ${message.payload.topic}<br>`;
+                        break;
+                }
+
+                break;
+            case "dat":
+                // Could push this to a div or a section based on payload.topic
+                statusBox.innerHTML += `${message.payload.topic}:${message.payload.msg}<br>`;
+                break;
+            default:
+                break;
+        }
+    });
+
+    sock.addEventListener("open", () =>
+    {
+        statusBox.innerHTML += `Connection opened<br>`;
+    });
+})();
